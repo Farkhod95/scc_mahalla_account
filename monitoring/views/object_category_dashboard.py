ec78f3f4615a26b53b37d4925f2af24b830bf2fa
Farkhod95 <misirovfarkhod@gmail.com>
2026-02-09 10:37:16 +0500

    WWW

--- monitoring/views/object_category_dashboard.py
+++ monitoring/views/object_category_dashboard.py
@@ -1,9 +1,5 @@
 # views.py
 from django.db.models import Count, Q
-from django.db.models.functions import Coalesce
-from django.db.models import IntegerField
-from django.db.models import FilteredRelation
-
 from rest_framework.generics import ListAPIView
 
 from monitoring.models import ObjectCategory
@@ -17,31 +13,20 @@ class ObjectCategoryForDashboardView(ListAPIView):
     def get_queryset(self):
         mahalla_id = self.request.query_params.get("mahalla_id")
 
-        qs = ObjectCategory.objects.all()
+        qs = ObjectCategory.objects.all().order_by("id")
 
-        # mahalla_id kelmasa: eski logika (hammasi bo‘yicha count)
+        # mahalla_id bo'lmasa: hammasi bo'yicha count
         if not mahalla_id:
-            return (
-                qs.annotate(objects_count=Count("category_objects", distinct=True))
-                  .order_by("id")
-            )
+            return qs.annotate(objects_count=Count("category_objects", distinct=True))
 
-        # mahalla_id bo‘lsa: Object larni organization__mahalla_id bo‘yicha filterlab count
-        # Eslatma: Agar sizda yo‘l boshqacha bo‘lsa, shu joyni moslang:
-        # masalan: category_objects__mahalla_id yoki organization__district_id va h.k.
-        return (
-            qs.annotate(
-                filtered_objects=FilteredRelation(
-                    "category_objects",
-                    condition=Q(category_objects__organization__mahalla_id=mahalla_id)
-                )
+        # mahalla_id bo'lsa: mahalla bo'yicha filterlangan count
+        # !!! Eslatma: bu yerda yo'l sizda shunaqa deb hisoblayapmiz:
+        # Object -> organization -> mahalla
+        # Agar sizda boshqacha bo'lsa, shu Q(...) ichidagi yo'lni moslang.
+        return qs.annotate(
+            objects_count=Count(
+                "category_objects",
+                filter=Q(category_objects__organization__mahalla_id=mahalla_id),
+                distinct=True,
             )
-            .annotate(
-                objects_count=Coalesce(
-                    Count("filtered_objects", distinct=True),
-                    0,
-                    output_field=IntegerField()
-                )
-            )
-            .order_by("id")
         )
