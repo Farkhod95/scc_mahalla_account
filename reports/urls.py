68c3382a0056c72a50541c84a16f3ba7fd298d7d
Farkhod95 <misirovfarkhod@gmail.com>
2026-03-03 14:06:34 +0500

    Malika shop

--- reports/urls.py
+++ reports/urls.py
@@ -4,7 +4,7 @@ from reports.views.detection_count_api import DetectionCountProxyView
 from reports.views.face_detection_count_api import FaceDetectionCountProxyView
 
 urlpatterns = [
-    path("reports/drf/detection-count/", DetectionCountProxyView.as_view(), name="drf-detection-count"),
+    path("reports/drb/detection-count/", DetectionCountProxyView.as_view(), name="drf-detection-count"),
     path("reports/face/detection-count", FaceDetectionCountProxyView.as_view(),
         name="face-detection-count-proxy",),
 ]
